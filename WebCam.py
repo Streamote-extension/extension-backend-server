c223e16007079fd8ceec89a8874a088b376a138f
Nhuq001 <nhuq001@fiu.edu>
2018-10-28 00:25:30 -0400

    Created VidCapture Class

---
+++ WebCam.py
@@ -0,0 +1,37 @@
+from threading import Thread
+import cv2
+
+
+class WebcamVideoStream:
+    def __init__(self, src=0):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopThread = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        # loop til thread stops
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
+
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        # return the frame most recently read
+        return self.frame
+
+    def stop(self):
+        # thread should stop
+        self.stopThread= True
